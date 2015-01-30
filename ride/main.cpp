3e748ccf77426581d80108355562b5309fa6fa96
Gustav <sir.gustav.the.coder@gmail.com>
2015-01-30 23:18:25 +0100

    don't crash when closing

--- ride/main.cpp
+++ ride/main.cpp
@@ -25,7 +25,9 @@ private:
 	void OnExit(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
 
-  void OnMarginClick(wxStyledTextEvent& event);
+  void OnNotebookPageClose(wxAuiNotebookEvent& evt);
+  void OnNotebookPageClosed(wxAuiNotebookEvent& evt);
+
 	wxDECLARE_EVENT_TABLE();
 
 private:
@@ -45,6 +47,9 @@ wxBEGIN_EVENT_TABLE(MainWindow, wxFrame)
 	EVT_MENU(ID_Hello, MainWindow::OnHello)
 	EVT_MENU(wxID_EXIT, MainWindow::OnExit)
 	EVT_MENU(wxID_ABOUT, MainWindow::OnAbout)
+
+  EVT_AUINOTEBOOK_PAGE_CLOSE(wxID_ANY, MainWindow::OnNotebookPageClose)
+  EVT_AUINOTEBOOK_PAGE_CLOSED(wxID_ANY, MainWindow::OnNotebookPageClosed)
 wxEND_EVENT_TABLE()
 
 wxIMPLEMENT_APP(MyApp);
@@ -79,6 +84,130 @@ MainWindow::MainWindow(const wxString& title, const wxPoint& pos, const wxSize& 
   setupEdit();
 }
 
+class EditCtrl : public wxControl {
+public:
+  EditCtrl(wxAuiNotebook* anotebook, wxWindow* parent) : wxControl(parent, wxID_ANY), notebook(anotebook) {
+    text = new wxStyledTextCtrl(this, wxID_ANY);
+
+    text->StyleClearAll();
+    text->SetLexer(wxSTC_LEX_CPP);
+
+    text->SetMarginWidth(MARGIN_LINE_NUMBERS, 50);
+    text->StyleSetForeground(wxSTC_STYLE_LINENUMBER, wxColour(75, 75, 75));
+    text->StyleSetBackground(wxSTC_STYLE_LINENUMBER, wxColour(220, 220, 220));
+    text->SetMarginType(MARGIN_LINE_NUMBERS, wxSTC_MARGIN_NUMBER);
+
+    // ---- Enable code folding
+    text->SetMarginType(MARGIN_FOLD, wxSTC_MARGIN_SYMBOL);
+    text->SetMarginWidth(MARGIN_FOLD, 15);
+    text->SetMarginMask(MARGIN_FOLD, wxSTC_MASK_FOLDERS);
+    text->StyleSetBackground(MARGIN_FOLD, wxColor(200, 200, 200));
+    text->SetMarginSensitive(MARGIN_FOLD, true);
+
+    // Properties found from http://www.scintilla.org/SciTEDoc.html
+    text->SetProperty(wxT("fold"), wxT("1"));
+    text->SetProperty(wxT("fold.comment"), wxT("1"));
+    text->SetProperty(wxT("fold.compact"), wxT("1"));
+
+    wxColor grey(100, 100, 100);
+    text->MarkerDefine(wxSTC_MARKNUM_FOLDER, wxSTC_MARK_ARROW);
+    text->MarkerSetForeground(wxSTC_MARKNUM_FOLDER, grey);
+    text->MarkerSetBackground(wxSTC_MARKNUM_FOLDER, grey);
+
+    text->MarkerDefine(wxSTC_MARKNUM_FOLDEROPEN, wxSTC_MARK_ARROWDOWN);
+    text->MarkerSetForeground(wxSTC_MARKNUM_FOLDEROPEN, grey);
+    text->MarkerSetBackground(wxSTC_MARKNUM_FOLDEROPEN, grey);
+
+    text->MarkerDefine(wxSTC_MARKNUM_FOLDERSUB, wxSTC_MARK_EMPTY);
+    text->MarkerSetForeground(wxSTC_MARKNUM_FOLDERSUB, grey);
+    text->MarkerSetBackground(wxSTC_MARKNUM_FOLDERSUB, grey);
+
+    text->MarkerDefine(wxSTC_MARKNUM_FOLDEREND, wxSTC_MARK_ARROW);
+    text->MarkerSetForeground(wxSTC_MARKNUM_FOLDEREND, grey);
+    text->MarkerSetBackground(wxSTC_MARKNUM_FOLDEREND, _T("WHITE"));
+
+    text->MarkerDefine(wxSTC_MARKNUM_FOLDEROPENMID, wxSTC_MARK_ARROWDOWN);
+    text->MarkerSetForeground(wxSTC_MARKNUM_FOLDEROPENMID, grey);
+    text->MarkerSetBackground(wxSTC_MARKNUM_FOLDEROPENMID, _T("WHITE"));
+
+    text->MarkerDefine(wxSTC_MARKNUM_FOLDERMIDTAIL, wxSTC_MARK_EMPTY);
+    text->MarkerSetForeground(wxSTC_MARKNUM_FOLDERMIDTAIL, grey);
+    text->MarkerSetBackground(wxSTC_MARKNUM_FOLDERMIDTAIL, grey);
+
+    text->MarkerDefine(wxSTC_MARKNUM_FOLDERTAIL, wxSTC_MARK_EMPTY);
+    text->MarkerSetForeground(wxSTC_MARKNUM_FOLDERTAIL, grey);
+    text->MarkerSetBackground(wxSTC_MARKNUM_FOLDERTAIL, grey);
+    // ---- End of code folding part
+
+    text->SetWrapMode(wxSTC_WRAP_WORD); // other choice is wxSCI_WRAP_NONE
+
+    // text->SetText(source);
+
+    text->StyleSetForeground(wxSTC_C_STRING, wxColour(150, 0, 0));
+    text->StyleSetForeground(wxSTC_C_PREPROCESSOR, wxColour(165, 105, 0));
+    text->StyleSetForeground(wxSTC_C_IDENTIFIER, wxColour(40, 0, 60));
+    text->StyleSetForeground(wxSTC_C_NUMBER, wxColour(0, 150, 0));
+    text->StyleSetForeground(wxSTC_C_CHARACTER, wxColour(150, 0, 0));
+    text->StyleSetForeground(wxSTC_C_WORD, wxColour(0, 0, 150));
+    text->StyleSetForeground(wxSTC_C_WORD2, wxColour(0, 150, 0));
+    text->StyleSetForeground(wxSTC_C_COMMENT, wxColour(150, 150, 150));
+    text->StyleSetForeground(wxSTC_C_COMMENTLINE, wxColour(150, 150, 150));
+    text->StyleSetForeground(wxSTC_C_COMMENTDOC, wxColour(150, 150, 150));
+    text->StyleSetForeground(wxSTC_C_COMMENTDOCKEYWORD, wxColour(0, 0, 200));
+    text->StyleSetForeground(wxSTC_C_COMMENTDOCKEYWORDERROR, wxColour(0, 0, 200));
+    text->StyleSetBold(wxSTC_C_WORD, true);
+    text->StyleSetBold(wxSTC_C_WORD2, true);
+    text->StyleSetBold(wxSTC_C_COMMENTDOCKEYWORD, true);
+
+    // a sample list of keywords, I haven't included them all to keep it short...
+    text->SetKeyWords(0, wxT("return for while break continue"));
+    text->SetKeyWords(1, wxT("const int float void char double"));
+
+    text->Connect(wxEVT_STC_MARGINCLICK, wxStyledTextEventHandler(EditCtrl::OnMarginClick), NULL, this);
+
+    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(text, 1, wxEXPAND);
+    SetSizer(sizer);
+
+    //size_t count = notebook->GetPageCount();
+    notebook->AddPage(this, wxT("document"));
+    //page = notebook->GetPage(count);
+  }
+
+  bool canClose() {
+    return true;
+  }
+
+  /** Event callback when a margin is clicked, used here for code folding */
+  void OnMarginClick(wxStyledTextEvent& event)
+  {
+    if (event.GetMargin() == MARGIN_FOLD)
+    {
+      int lineClick = text->LineFromPosition(event.GetPosition());
+      int levelClick = text->GetFoldLevel(lineClick);
+
+      if ((levelClick & wxSTC_FOLDLEVELHEADERFLAG) > 0)
+      {
+        text->ToggleFold(lineClick);
+      }
+    }
+  }
+
+  ~EditCtrl() {
+    text->SetClientData(NULL);
+  }
+
+  wxStyledTextCtrl* text;
+  wxAuiNotebook* notebook;
+  //wxWindow* page;
+
+private:
+  wxDECLARE_EVENT_TABLE();
+};
+
+wxBEGIN_EVENT_TABLE(EditCtrl, wxControl)
+wxEND_EVENT_TABLE()
+
 void MainWindow::createNotebook() {
   wxSize client_size = GetClientSize();
   wxAuiNotebook* ctrl = new wxAuiNotebook(this, wxID_ANY,
@@ -97,102 +226,7 @@ MainWindow::~MainWindow() {
 }
 
 void MainWindow::setupEdit() {
-  wxStyledTextCtrl* text = new wxStyledTextCtrl(this, wxID_ANY);
-
-  text->StyleClearAll();
-  text->SetLexer(wxSTC_LEX_CPP);
-
-  text->SetMarginWidth(MARGIN_LINE_NUMBERS, 50);
-  text->StyleSetForeground(wxSTC_STYLE_LINENUMBER, wxColour(75, 75, 75));
-  text->StyleSetBackground(wxSTC_STYLE_LINENUMBER, wxColour(220, 220, 220));
-  text->SetMarginType(MARGIN_LINE_NUMBERS, wxSTC_MARGIN_NUMBER);
-
-
-  // ---- Enable code folding
-  text->SetMarginType(MARGIN_FOLD, wxSTC_MARGIN_SYMBOL);
-  text->SetMarginWidth(MARGIN_FOLD, 15);
-  text->SetMarginMask(MARGIN_FOLD, wxSTC_MASK_FOLDERS);
-  text->StyleSetBackground(MARGIN_FOLD, wxColor(200, 200, 200));
-  text->SetMarginSensitive(MARGIN_FOLD, true);
-
-  // Properties found from http://www.scintilla.org/SciTEDoc.html
-  text->SetProperty(wxT("fold"), wxT("1"));
-  text->SetProperty(wxT("fold.comment"), wxT("1"));
-  text->SetProperty(wxT("fold.compact"), wxT("1"));
-
-  wxColor grey(100, 100, 100);
-  text->MarkerDefine(wxSTC_MARKNUM_FOLDER, wxSTC_MARK_ARROW);
-  text->MarkerSetForeground(wxSTC_MARKNUM_FOLDER, grey);
-  text->MarkerSetBackground(wxSTC_MARKNUM_FOLDER, grey);
-
-  text->MarkerDefine(wxSTC_MARKNUM_FOLDEROPEN, wxSTC_MARK_ARROWDOWN);
-  text->MarkerSetForeground(wxSTC_MARKNUM_FOLDEROPEN, grey);
-  text->MarkerSetBackground(wxSTC_MARKNUM_FOLDEROPEN, grey);
-
-  text->MarkerDefine(wxSTC_MARKNUM_FOLDERSUB, wxSTC_MARK_EMPTY);
-  text->MarkerSetForeground(wxSTC_MARKNUM_FOLDERSUB, grey);
-  text->MarkerSetBackground(wxSTC_MARKNUM_FOLDERSUB, grey);
-
-  text->MarkerDefine(wxSTC_MARKNUM_FOLDEREND, wxSTC_MARK_ARROW);
-  text->MarkerSetForeground(wxSTC_MARKNUM_FOLDEREND, grey);
-  text->MarkerSetBackground(wxSTC_MARKNUM_FOLDEREND, _T("WHITE"));
-
-  text->MarkerDefine(wxSTC_MARKNUM_FOLDEROPENMID, wxSTC_MARK_ARROWDOWN);
-  text->MarkerSetForeground(wxSTC_MARKNUM_FOLDEROPENMID, grey);
-  text->MarkerSetBackground(wxSTC_MARKNUM_FOLDEROPENMID, _T("WHITE"));
-
-  text->MarkerDefine(wxSTC_MARKNUM_FOLDERMIDTAIL, wxSTC_MARK_EMPTY);
-  text->MarkerSetForeground(wxSTC_MARKNUM_FOLDERMIDTAIL, grey);
-  text->MarkerSetBackground(wxSTC_MARKNUM_FOLDERMIDTAIL, grey);
-
-  text->MarkerDefine(wxSTC_MARKNUM_FOLDERTAIL, wxSTC_MARK_EMPTY);
-  text->MarkerSetForeground(wxSTC_MARKNUM_FOLDERTAIL, grey);
-  text->MarkerSetBackground(wxSTC_MARKNUM_FOLDERTAIL, grey);
-  // ---- End of code folding part
-
-  text->SetWrapMode(wxSTC_WRAP_WORD); // other choice is wxSCI_WRAP_NONE
-
-  // text->SetText(source);
-
-  text->StyleSetForeground(wxSTC_C_STRING, wxColour(150, 0, 0));
-  text->StyleSetForeground(wxSTC_C_PREPROCESSOR, wxColour(165, 105, 0));
-  text->StyleSetForeground(wxSTC_C_IDENTIFIER, wxColour(40, 0, 60));
-  text->StyleSetForeground(wxSTC_C_NUMBER, wxColour(0, 150, 0));
-  text->StyleSetForeground(wxSTC_C_CHARACTER, wxColour(150, 0, 0));
-  text->StyleSetForeground(wxSTC_C_WORD, wxColour(0, 0, 150));
-  text->StyleSetForeground(wxSTC_C_WORD2, wxColour(0, 150, 0));
-  text->StyleSetForeground(wxSTC_C_COMMENT, wxColour(150, 150, 150));
-  text->StyleSetForeground(wxSTC_C_COMMENTLINE, wxColour(150, 150, 150));
-  text->StyleSetForeground(wxSTC_C_COMMENTDOC, wxColour(150, 150, 150));
-  text->StyleSetForeground(wxSTC_C_COMMENTDOCKEYWORD, wxColour(0, 0, 200));
-  text->StyleSetForeground(wxSTC_C_COMMENTDOCKEYWORDERROR, wxColour(0, 0, 200));
-  text->StyleSetBold(wxSTC_C_WORD, true);
-  text->StyleSetBold(wxSTC_C_WORD2, true);
-  text->StyleSetBold(wxSTC_C_COMMENTDOCKEYWORD, true);
-
-  // a sample list of keywords, I haven't included them all to keep it short...
-  text->SetKeyWords(0, wxT("return for while break continue"));
-  text->SetKeyWords(1, wxT("const int float void char double"));
-
-  text->Connect(wxEVT_STC_MARGINCLICK, wxStyledTextEventHandler(MainWindow::OnMarginClick), text, this);
-
-  notebook->AddPage(text, wxT("document"));
-}
-
-/** Event callback when a margin is clicked, used here for code folding */
-void MainWindow::OnMarginClick(wxStyledTextEvent& event)
-{
-  wxStyledTextCtrl* ctrl = reinterpret_cast<wxStyledTextCtrl*>(event.GetEventUserData());
-  if (event.GetMargin() == MARGIN_FOLD)
-  {
-    int lineClick = ctrl->LineFromPosition(event.GetPosition());
-    int levelClick = ctrl->GetFoldLevel(lineClick);
-
-    if ((levelClick & wxSTC_FOLDLEVELHEADERFLAG) > 0)
-    {
-      ctrl->ToggleFold(lineClick);
-    }
-  }
+  new EditCtrl(notebook, this);
 }
 
 void MainWindow::OnExit(wxCommandEvent& event)
@@ -208,4 +242,17 @@ void MainWindow::OnAbout(wxCommandEvent& event)
 void MainWindow::OnHello(wxCommandEvent& event)
 {
 	wxLogMessage("Hello world from wxWidgets!");
+}
+
+void MainWindow::OnNotebookPageClose(wxAuiNotebookEvent& evt) {
+  wxWindow* window = notebook->GetPage(evt.GetSelection());
+  if (window->IsKindOf(CLASSINFO(EditCtrl))) {
+    EditCtrl* edit = reinterpret_cast<EditCtrl*>(window);
+    if (edit->canClose() == false) {
+      evt.Veto();
+    }
+  }
+}
+
+void MainWindow::OnNotebookPageClosed(wxAuiNotebookEvent& evt) {
 }
