8c07418300defe3744b9f48a080d0ffd9e810280
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-05 07:20:13 +0100

    enum settings in dialog

--- ride/settingsdlg.cpp
+++ ride/settingsdlg.cpp
@@ -45,20 +45,48 @@ ride::Color ToData(wxColourPickerCtrl* gui)  {
   return C(gui->GetColour());
 }
 
+#define RETURN_COMBOBOX_VALUE(TYPE, VALUE) assert(ride::TYPE##_IsValid(VALUE)); return static_cast<ride::TYPE>(VALUE)
+
+void ToGui(ride::ViewWhitespace data, wxComboBox* gui)  {
+  gui->SetSelection(static_cast<int>(data));
+}
+ride::ViewWhitespace ToData_VW(wxComboBox* gui)  {
+  RETURN_COMBOBOX_VALUE(ViewWhitespace, gui->GetSelection());
+}
+
+void ToGui(ride::WrapMode data, wxComboBox* gui)  {
+  gui->SetSelection(static_cast<int>(data));
+}
+ride::WrapMode ToData_WM(wxComboBox* gui)  {
+  RETURN_COMBOBOX_VALUE(WrapMode, gui->GetSelection());
+}
+
+void ToGui(ride::EdgeStyle data, wxComboBox* gui)  {
+  gui->SetSelection(static_cast<int>(data));
+}
+ride::EdgeStyle ToData_ES(wxComboBox* gui)  {
+  RETURN_COMBOBOX_VALUE(EdgeStyle, gui->GetSelection());
+}
+
 template<typename T>
 T* Allocate(const T& t) {
   return new T(t);
 }
 
-#define DIALOG_DATA(ROOT, FUN, UI) do { if( togui ) { ToGui(ROOT.FUN(), UI); } else { ROOT.set_##FUN(ToData(UI)); } } while(false)
+#define DIALOG_DATA(ROOT, FUN, UI, SETNAME) do { if( togui ) { ToGui(ROOT.FUN(), UI); } else { ROOT.set_##FUN(ToData##SETNAME(UI)); } } while(false)
 #define DIALOG_DATAX(ROOT, FUN, UI) do { if( togui ) { ToGui(ROOT.FUN(), UI); } else { ROOT.set_allocated_##FUN(Allocate(ToData(UI))); } } while(false)
 
 void SettingsDlg::editToGui(bool togui) {
   ride::FontsAndColors fonts_and_colors = edit.fonts_and_colors();
 
-  DIALOG_DATA(edit, displayeolenable, uiDisplayEOL);
-  DIALOG_DATA(edit, linenumberenable, uiShowLineNumbers);
-  DIALOG_DATA(edit, indentguideenable, uiIndentGuide);
+  DIALOG_DATA(edit, displayeolenable, uiDisplayEOL,);
+  DIALOG_DATA(edit, linenumberenable, uiShowLineNumbers,);
+  DIALOG_DATA(edit, indentguideenable, uiIndentGuide,);
+
+  DIALOG_DATA(edit, whitespace, uiViewWhitespace, _VW);
+  DIALOG_DATA(edit, wordwrap, uiWordwrap, _WM);
+  DIALOG_DATA(edit, edgestyle, uiEdgeStyle, _ES);
+
   DIALOG_DATAX(fonts_and_colors, edgecolor, uiEdgeColor);
 
   if (togui == false) {
