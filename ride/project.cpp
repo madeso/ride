736ebf108b7efda406bb43e310ea6c55bfe672af
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-14 22:50:44 +0100

    Added dummy project functions

--- ride/project.cpp
+++ ride/project.cpp
@@ -7,3 +7,30 @@ Project::Project(const wxString& root_folder) : root_folder_(root_folder) {
 const wxString& Project::root_folder() const {
   return root_folder_;
 }
+
+void Project::Settings() {
+}
+
+void Project::Build() {
+}
+
+void Project::Clean() {
+}
+
+void Project::Rebuild() {
+}
+
+void Project::Doc() {
+}
+
+void Project::Run() {
+}
+
+void Project::Test() {
+}
+
+void Project::Bench() {
+}
+
+void Project::Update() {
+}
--- ride/project.h
+++ ride/project.h
@@ -8,6 +8,16 @@ public:
   Project(const wxString& root_folder);
   const wxString& root_folder() const;
 
+  void Settings();
+  void Build   ();
+  void Clean   ();
+  void Rebuild ();
+  void Doc     ();
+  void Run     ();
+  void Test    ();
+  void Bench   ();
+  void Update  ();
+
 private:
   wxString root_folder_;
 };
