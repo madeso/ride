f2332ab48406ad21597fe8033fec651850f95489
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-15 19:19:01 +0100

    added todo

--- ride/fileedit.cpp
+++ ride/fileedit.cpp
@@ -648,12 +648,15 @@ void FileEdit::OnCharAdded(wxStyledTextEvent& event)
   // loosley based on http://www.scintilla.org/ScintillaUsage.html and https://groups.google.com/forum/#!topic/scintilla-interest/vTwXwIBswSM
   int chr = event.GetKey(); // the key seems to be the char that was added
   if (chr == '{') {
+    // todo: setting for this
     text->InsertText(text->GetCurrentPos(), "}");
   }
   else if (chr == '(') {
+    // todo: setting for this
     text->InsertText(text->GetCurrentPos(), ")");
   }
   else if (chr == '[') {
+    // todo: setting for this
     text->InsertText(text->GetCurrentPos(), "]");
   }
   else if (chr == '\n' || chr == '\r')
