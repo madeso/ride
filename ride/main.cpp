0a8ac5e41f552ea08cb6df2a7df138e5ea0b7402
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-17 22:27:54 +0100

    Cleanup

--- ride/main.cpp
+++ ride/main.cpp
@@ -15,13 +15,15 @@ bool MyApp::OnInit()
 {
   GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-  wxAppConsole* c = wxAppConsole::GetInstance();
-  c->SetAppName("ride");
-  c->SetAppDisplayName("RIDE");
-  c->SetVendorName("madeso");
-  c->SetVendorDisplayName("madeso");
+  const wxString DISPLAY_NAME = "Ride";
 
-	MainWindow *frame = new MainWindow("RIDE - Rust IDE", wxPoint(50, 50), wxSize(450, 340) );
-	frame->Show( true );
+  wxAppConsole* app_console = wxAppConsole::GetInstance();
+  app_console->SetAppName("ride");
+  app_console->SetAppDisplayName(DISPLAY_NAME);
+  app_console->SetVendorName("madeso");
+  app_console->SetVendorDisplayName("madeso");
+
+  MainWindow *main_window = new MainWindow(DISPLAY_NAME, wxPoint(50, 50), wxSize(450, 340));
+	main_window->Show( true );
 	return true;
 }
