36e7dce16000c9e523da08d00f8fac091b261e66
Gustav <sir.gustav.the.coder@gmail.com>
2015-02-27 18:41:58 +0100

    converted icons to xpm

---
+++ ride/resources/icons.h
@@ -0,0 +1,29 @@
+#ifndef RIDE_RIDE_RESOURCES_ICON_H
+#define RIDE_RIDE_RESOURCES_ICON_H
+
+#include "icons/edit_copy.xpm"
+#include "icons/edit_cut.xpm"
+#include "icons/edit_make_lowercase.xpm"
+#include "icons/edit_make_uppercase.xpm"
+#include "icons/edit_paste.xpm"
+#include "icons/edit_redo.xpm"
+#include "icons/edit_tab_add.xpm"
+#include "icons/edit_tab_remove.xpm"
+#include "icons/edit_undo.xpm"
+#include "icons/file_normal.xpm"
+#include "icons/file_warning.xpm"
+#include "icons/folder_error.xpm"
+#include "icons/folder_normal.xpm"
+#include "icons/folder_warning.xpm"
+#include "icons/project_bench.xpm"
+#include "icons/project_build.xpm"
+#include "icons/project_clean.xpm"
+#include "icons/project_debug.xpm"
+#include "icons/project_doc.xpm"
+#include "icons/project_rebuild.xpm"
+#include "icons/project_run.xpm"
+#include "icons/project_stop.xpm"
+#include "icons/project_test.xpm"
+#include "icons/project_update.xpm"
+
+#endif  // RIDE_RIDE_RESOURCES_ICON_H
