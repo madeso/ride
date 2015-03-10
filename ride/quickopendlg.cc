6b6c56b8103e0808a27dc5cbb3c819ad288bdf27
Gustav <sir.gustav.the.coder@gmail.com>
2015-03-10 23:58:36 +0100

    started on matching

--- ride/quickopendlg.cc
+++ ride/quickopendlg.cc
@@ -2,6 +2,7 @@
 
 #include "ride/wx.h"
 #include "ride/generated/ui.h"
+#include "ride/wxutils.h"
 #include <vector>
 
 // based on http://docs.wholetomato.com/default.asp?W193
@@ -21,8 +22,39 @@ protected:
   std::vector<wxString> files_;
 };
 
-bool MatchFilter(const wxString& filter, const wxString file) {
-  return file.EndsWith(filter);
+void add_if_more_than_one(std::vector<wxString>& ret, const std::vector<wxString>& space) {
+  if (space.size() != 1) {
+    ret.insert(ret.begin(), space.begin(), space.end());
+  }
+}
+
+std::vector<wxString> SmartSplit(const wxString str) {
+  const auto space = Split(str, ' ');
+  const auto dash = Split(str, '-');
+  const auto under = Split(str, '_');
+  std::vector<wxString> ret;
+  ret.push_back(str);
+
+  add_if_more_than_one(ret, space);
+  add_if_more_than_one(ret, dash);
+  add_if_more_than_one(ret, under);
+
+  return ret;
+}
+
+bool MatchFilter(const wxString& filter, const wxString file, int* count) {
+  const wxFileName name(file); 
+  const auto filters = Split(filter, ' ');
+  const auto bn = SmartSplit(name.GetFullName());
+  if (filters.empty()) return true;
+  for (auto f : filters) {
+    for (auto b : bn) {
+      if (b.StartsWith(f)) {
+        *count += 1;
+      }
+    }
+  }
+  return *count > 0;
 }
 
 void QuickOpenDlg::UpdateFilters() {
@@ -31,10 +63,12 @@ void QuickOpenDlg::UpdateFilters() {
   uiFileList->Freeze();
   uiFileList->DeleteAllItems();
   for (const wxString& file : files_) {
-    if (MatchFilter(filter, file)) {
+    int count = 0;
+    if (MatchFilter(filter, file, &count)) {
       int i = uiFileList->InsertItem(0, "");
       uiFileList->SetItem(i, 0, wxFileName(file).GetFullName());
       uiFileList->SetItem(i, 1, file);
+      uiFileList->SetItem(i, 2, wxString::Format("%d", count));
     }
   }
   uiFileList->Thaw();
@@ -45,25 +79,28 @@ QuickOpenDlg::QuickOpenDlg(wxWindow* parent)
 {
   const long file_index = uiFileList->InsertColumn(0, "File");
   const long path_index = uiFileList->InsertColumn(1, "Path");
-  uiFileList->SetColumnWidth(file_index, 100);
-  uiFileList->SetColumnWidth(path_index, 200);
+  const long count_index = uiFileList->InsertColumn(2, "Hits");
+  uiFileList->SetColumnWidth(file_index, 150);
+  uiFileList->SetColumnWidth(path_index, 300);
+  uiFileList->SetColumnWidth(count_index, 50);
 
-  files_.push_back("/project/dog/src/BigCalendarCtrl.rs");
+  /*files_.push_back("/project/dog/src/BigCalendarCtrl.rs");
   files_.push_back("/project/dog/src/BigCalendarTask.rs");
   files_.push_back("/project/dog/src/CalendarButtonsDlg.rs");
   files_.push_back("/project/dog/src/CalendarData.rs");
   files_.push_back("/project/dog/src/CalendarDefines.rs");
   files_.push_back("/project/dog/src/CalendarExt.rs");
   files_.push_back("/project/dog/src/CalendarExt.rs");
+  */
 
-  /*files_.push_back("/project/dog/src/big-calendar-ctrl.rs");
+  files_.push_back("/project/dog/src/big-calendar-ctrl.rs");
   files_.push_back("/project/dog/src/big-calendar-task.rs");
   files_.push_back("/project/dog/src/calendar-buttons-dlg.rs");
   files_.push_back("/project/dog/src/calendar-data.rs");
   files_.push_back("/project/dog/src/calendar-defines.rs");
   files_.push_back("/project/dog/src/calendar-ext.rs");
   files_.push_back("/project/dog/src/calendar-ext.rs");
-
+  /*
   files_.push_back("/project/dog/src/big_calendar_ctrl.rs");
   files_.push_back("/project/dog/src/big_calendar_task.rs");
   files_.push_back("/project/dog/src/calendar_buttons_dlg.rs");
