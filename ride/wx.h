e79327f0dd72256f8d98b8c15833957d95d5324a
Gustav <sir.gustav.the.coder@gmail.com>
2015-05-09 19:13:24 +0200

    removed warning...

--- ride/wx.h
+++ ride/wx.h
@@ -8,10 +8,14 @@
 #include <ciso646>  // detect std::lib
 #ifdef _LIBCPP_VERSION
 // using libc++
+#ifndef HAVE_TYPE_TRAITS
 #define HAVE_TYPE_TRAITS
+#endif
 #else
 // using libstdc++
+#ifndef HAVE_TR1_TYPE_TRAITS
 #define HAVE_TR1_TYPE_TRAITS
+#endif
 #endif
 
 #include <wx/wxprec.h>
