68a66ad7707fe9a669c63d76193d18ac2a48b92a
Gustav <sir.gustav.the.coder@gmail.com>
2015-07-25 11:33:21 +0200

    reading version and authors from cargo file too

--- ride/cargo.cc
+++ ride/cargo.cc
@@ -1,7 +1,32 @@
 #include "ride/cargo.h"
 #include "cpptoml.h"
+#include <string>
 
 //////////////////////////////////////////////////////////////////////////
+
+// cpptoml doesn't support wxString, only std::string
+template<typename T>
+std::shared_ptr<T> GetValue(const std::shared_ptr<cpptoml::base> base) {
+  const auto t = base->as<T>();
+  if (t.get() == NULL) {
+    // if null, return null
+    return std::shared_ptr<T>();
+  }
+  return std::shared_ptr<T>(new T(t->get()));
+}
+
+// so the specialization solves it by using a temporary std::string
+template<>
+std::shared_ptr<wxString> GetValue<wxString>(const std::shared_ptr<cpptoml::base> base)
+{
+  const auto str = GetValue<std::string>(base);
+  if (str.get() == NULL) {
+    // if null, return null
+    return std::shared_ptr<wxString>();
+  }
+  return std::shared_ptr<wxString>(new wxString(*str.get()));
+}
+
 template <typename T>
 LoadResult SafeGet(const std::string& table_name, std::shared_ptr<cpptoml::table> table, const std::string& name, T* result)
 {
@@ -16,19 +41,52 @@ LoadResult SafeGet(const std::string& table_name, std::shared_ptr<cpptoml::table
       "%s from table %s is not a value",
       name, table_name));
   }
-  const auto value = base->as<T>();
-  *result = value->get();
+  const auto value = GetValue<T>(base);
+  if (value.get() == NULL) {
+    return LoadResult::Error(wxString::Format(
+      "%s from table %s is not a valid value",
+      name, table_name));
+  }
+  *result = *value;
   return LoadResult::Ok();
 }
 
-// cpptoml doesn't support wxString, only std::string so lets use a specialization with a temporary std::string
-template<>
-LoadResult SafeGet<wxString>(const std::string& table_name, std::shared_ptr<cpptoml::table> table, const std::string& name, wxString* result)
+// array specialization
+template<typename T>
+LoadResult SafeGet(const std::string& table_name, std::shared_ptr<cpptoml::table> table, const std::string& name, std::vector<T>* result)
 {
-  std::string temp;
-  const auto ret = SafeGet(table_name, table, name, &temp);
-  *result = temp;
-  return ret;
+  result->resize(0);
+  if (false == table->contains(name)) {
+    return LoadResult::Error(wxString::Format(
+      "table %s doesn't contain %s",
+      table_name, name));
+  }
+  const auto array = table->get_array(name);
+  if (array.get() == NULL) {
+    return LoadResult::Error(wxString::Format(
+      "%s from table %s is not a array",
+      name, table_name));
+  }
+
+  int index = 0;
+  for (const auto base : array->get()) {
+    if (false == base->is_value()) {
+      return LoadResult::Error(wxString::Format(
+        "value %d in array %s from table %s is not a value",
+        index, name, table_name));
+    }
+    const auto value = GetValue<T>(base);
+    if (NULL == value.get()) {
+      return LoadResult::Error(wxString::Format(
+        "value %d in array %s from table %s is not a valid value",
+        index, name, table_name));
+    }
+
+    result->push_back(*value);
+    ++index;
+  }
+
+  return LoadResult::Ok();
 }
 
 // like the rust try macro
@@ -40,6 +98,8 @@ namespace cargo
 {
   const std::string PACKAGE = "package";
   const std::string NAME = "name";
+  const std::string VERSION = "version";
+  const std::string AUTHORS = "authors";
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -56,6 +116,8 @@ LoadResult Cargo::Load(const wxString& file)
     if ( package.get() == NULL ) return LoadResult::Error("No package in cargo file");
 
     TRY(SafeGet(cargo::PACKAGE, package, cargo::NAME, &name_));
+    TRY(SafeGet(cargo::PACKAGE, package, cargo::VERSION, &version_));
+    TRY(SafeGet(cargo::PACKAGE, package, cargo::AUTHORS, &authors_));
     return LoadResult::Ok();
   }
   catch (const cpptoml::parse_exception& e)
@@ -72,4 +134,24 @@ const wxString& Cargo::name() const
 void Cargo::set_name(const wxString& name)
 {
   name_ = name;
+}
+
+const wxString& Cargo::version() const
+{
+  return version_;
+}
+
+void Cargo::set_version(const wxString& version)
+{
+  version_ = version;
+}
+
+const std::vector<wxString>& Cargo::authors() const
+{
+  return authors_;
+}
+
+void Cargo::set_authors(const std::vector<wxString>& authors)
+{
+  authors_ = authors;
 }
--- ride/cargo.h
+++ ride/cargo.h
@@ -4,6 +4,8 @@
 #include "ride/wx.h"
 #include "ride/loadresult.h"
 
+#include <vector>
+
 class Cargo {
 public:
   LoadResult Load(const wxString& file);
@@ -11,8 +13,15 @@ public:
   const wxString& name() const;
   void set_name(const wxString& name);
 
+  const wxString& version() const;
+  void set_version(const wxString& version);
+
+  const std::vector<wxString>& authors() const;
+  void set_authors(const std::vector<wxString>& authors);
 private:
   wxString name_;
+  wxString version_;
+  std::vector<wxString> authors_;
 };
 
 #endif  // CARGO_H
