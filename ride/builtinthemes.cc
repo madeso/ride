86e80a5b0c57b7bf814e857292df3354f9b7e58d
Gustav <sir.gustav.the.coder@gmail.com>
2015-09-24 18:36:50 +0200

    added default template

--- ride/builtinthemes.cc
+++ ride/builtinthemes.cc
@@ -55,13 +55,15 @@ ride::Style* NewStyle(ride::Color* front, ride::Color* back = NULL,
   return style;
 }
 
+//////////////////////////////////////////////////////////////////////////
+
 void SetupDefaultTemplate(ride::FontsAndColors* colors) {
   colors->set_use_selection_background(true);
   colors->set_use_selection_foreground(true);
   colors->set_allocated_selection_foreground(NewColor(255, 255, 255));
   colors->set_allocated_selection_background(NewColor(0, 0, 0));
 
-  colors->set_allocated_default_style(NewStyle(NULL, NewColor(211)));
+  colors->set_allocated_default_style(NewStyle(NewColor(0), NewColor(211)));
 
   colors->set_allocated_selected_line(NewColor(255, 255, 0));  // yellow
   colors->set_allocated_style_comment(NewStyle(NewColor(0, 255, 0)));
@@ -71,6 +73,8 @@ void SetupDefaultTemplate(ride::FontsAndColors* colors) {
   colors->set_allocated_style_keyword(
       NewStyle(NewColor(0, 0, 255), NULL, true));
 }
+
+//////////////////////////////////////////////////////////////////////////
 
 // solarized colors from http://ethanschoonover.com/solarized
 const ride::Color solarized_base03 = MakeColor(0, 43, 54);
@@ -90,12 +94,21 @@ const ride::Color solarized_blue = MakeColor(38, 139, 210);
 const ride::Color solarized_cyan = MakeColor(42, 161, 152);
 const ride::Color solarized_green = MakeColor(133, 153, 0);
 
+//////////////////////////////////////////////////////////////////////////
+
 void AddBuiltInThemes(::ride::Settings* settings) {
   ThemeList* themes = settings->mutable_themes();
 
   const std::string RIDE_AUTHOR = "ride-default";
 
-  ride::Theme* temp = GetOrCreateTheme(themes, "Ride (default)");
-  temp->set_author(RIDE_AUTHOR);
-  SetupDefaultTemplate(temp->mutable_data());
+  ride::Theme* default_theme = GetOrCreateTheme(themes, "Ride (default)");
+  default_theme->set_author(RIDE_AUTHOR);
+  SetupDefaultTemplate(default_theme->mutable_data());
+
+  if (false == settings->has_fonts_and_colors()) {
+    // if the current settings is missing the fonts and colors
+    // apply the default theme
+    settings->set_allocated_fonts_and_colors(
+      new ride::FontsAndColors(default_theme->data()));
+  }
 }
