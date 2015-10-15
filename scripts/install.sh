e2cb57bb36f014a98f42802a518c6be56d811d33
Gustav <sir.gustav.the.coder@gmail.com>
2015-10-15 21:31:41 +0200

    revering to the last sudo cmd

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 set -ev
 
+echo $CXX
+
 #install ride dependencies
-add-apt-repository -y ppa:chris-lea/protobuf
-apt-get update -qq
-apt-get install -qq --assume-yes libgtk2.0-dev libprotobuf-dev protobuf-compiler libwebkit-dev graphviz doxygen
+sudo add-apt-repository -y ppa:chris-lea/protobuf
+sudo apt-get update -qq
+sudo apt-get install -qq --assume-yes libgtk2.0-dev libprotobuf-dev protobuf-compiler libwebkit-dev graphviz doxygen
 
 # update cmake and compiler: copied from install part of https://github.com/skystrife/cpptoml/blob/toml-v0.4.0/.travis.yml
-apt-get install libc6-i386
+sudo apt-get install libc6-i386
 wget http://www.cmake.org/files/v3.3/cmake-3.3.2-Linux-i386.sh
-sh cmake-3.3.2-Linux-i386.sh --prefix=/usr/local --exclude-subdir
+sudo sh cmake-3.3.2-Linux-i386.sh --prefix=/usr/local --exclude-subdir
 
 # credit: https://github.com/beark/ftl/
 # install g++ 4.8, if tests are run with g++
 if [ "`echo $CXX`" == "g++" ]; then
-	add-apt-repository -y ppa:ubuntu-toolchain-r/test
-	apt-get update
-	apt-get install -qq g++-4.8
-	update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test;
+	sudo apt-get update;
+	sudo apt-get install -qq g++-4.8;
+	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 fi
 
 # build wxWidgtets
@@ -32,5 +34,5 @@ cd gtk-build
 ../configure --help
 ../configure --enable-webview --disable-compat28
 make
-make install
+sudo make install
 wx-config --version
