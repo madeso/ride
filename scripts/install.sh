a9b9e093615a6cf32219430266dc89a2dd628949
Gustav <sir.gustav.the.coder@gmail.com>
2015-10-15 23:12:03 +0200

    try to run some commands as a regular user instead

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,16 @@ echo $CC
 #are commands even run?
 ls
 
+#from http://unix.stackexchange.com/questions/190571/sudo-in-non-interactive-script
+## Detect the user who launched the script
+usr=$(env | grep SUDO_USER | cut -d= -f 2)
+
+## Exit if the script was not launched by root or through sudo
+if [ -z $usr ] && [ $USER = "root" ]
+then
+    echo "The script needs to run as root" && exit 1
+fi
+
 #install ride dependencies
 add-apt-repository -y ppa:chris-lea/protobuf
 apt-get update -qq
@@ -15,7 +25,7 @@ apt-get install -qq --assume-yes libgtk2.0-dev libprotobuf-dev protobuf-compiler
 
 # update cmake and compiler: copied from install part of https://github.com/skystrife/cpptoml/blob/toml-v0.4.0/.travis.yml
 apt-get install libc6-i386
-wget http://www.cmake.org/files/v3.3/cmake-3.3.2-Linux-i386.sh
+sudo -u $usr wget http://www.cmake.org/files/v3.3/cmake-3.3.2-Linux-i386.sh
 sh cmake-3.3.2-Linux-i386.sh --prefix=/usr/local --exclude-subdir
 
 # credit: https://github.com/beark/ftl/
@@ -50,16 +60,16 @@ fi
 # fi
 
 # build wxWidgtets
-mkdir wx3
-cd wx3
-wget https://github.com/wxWidgets/wxWidgets/archive/WX_3_0_2.tar.gz -O wx.tar.gz
-tar -xzf wx.tar.gz
-ls
-cd wxWidgets-WX_3_0_2
-mkdir gtk-build
-cd gtk-build
-../configure --help
-../configure --enable-webview --disable-compat28
-make
+sudo -u $usr mkdir wx3
+sudo -u $usr cd wx3
+sudo -u $usr wget https://github.com/wxWidgets/wxWidgets/archive/WX_3_0_2.tar.gz -O wx.tar.gz
+sudo -u $usr tar -xzf wx.tar.gz
+sudo -u $usr ls
+sudo -u $usr cd wxWidgets-WX_3_0_2
+sudo -u $usr mkdir gtk-build
+sudo -u $usr cd gtk-build
+sudo -u $usr ../configure --help
+sudo -u $usr ../configure --enable-webview --disable-compat28
+sudo -u $usr make
 make install
-wx-config --version
+sudo -u $usr wx-config --version
