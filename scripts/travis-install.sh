7dfb486242f25663eba1ee745a6074602c14527f
Gustav <sir.gustav.the.coder@gmail.com>
2016-05-27 19:44:00 +0200

    cleanup

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -ev
 
-# what is the current compiler?
-echo $CXX
-echo $CC
-# which $CC
-
-$CXX --version
-$CC --version
-
 # update and display enviroment variables
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export LD_RUN_PATH=$LD_RUN_PATH:/usr/local/lib
@@ -20,12 +12,6 @@ ls
 #from http://unix.stackexchange.com/questions/190571/sudo-in-non-interactive-script
 ## Detect the user who launched the script
 usr=$(env | grep SUDO_USER | cut -d= -f 2)
-
-## Exit if the script was not launched by root or through sudo
-if [ -z $usr ] && [ $USER = "root" ]
-then
-    echo "The script needs to run as root" && exit 1
-fi
 
 #install ride dependencies
 sudo apt-get update -qq
@@ -39,10 +25,10 @@ sudo sh cmake-3.3.2-Linux-i386.sh --prefix=/usr/local --exclude-subdir
 # credit: https://github.com/beark/ftl/
 # install g++ 4.8, if tests are run with g++
 if [ "`echo $CXX`" == "g++" ]; then
-	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test;
-	sudo apt-get update -qq;
-	sudo apt-get install -qq g++-4.8;
-	sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test;
+    sudo apt-get update -qq;
+    sudo apt-get install -qq g++-4.8;
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
     export CXX="g++-4.8" CC="gcc-4.8"
 fi
 
@@ -62,9 +48,16 @@ make check &> proto_check || cat proto_check
 sudo make install
 
 # build wxWidgtets
-sudo -i $TRAVIS_BUILD_DIR/scripts/build-wx.sh
-
-cd $TRAVIS_BUILD_DIR/wx3/wxWidgets-3.1.0/gtk-build/
+cd $TRAVIS_BUILD_DIR
+mkdir wx3
+cd wx3
+wget https://github.com/wxWidgets/wxWidgets/archive/v3.1.0.tar.gz -O wx.tar.gz
+tar -xzf wx.tar.gz &> wxtar || cat wxtar
+cd wxWidgets-3.1.0
+mkdir gtk-build
+cd gtk-build
+../configure --enable-webview --disable-compat28
+make
 sudo make install
 wx-config --version
 
