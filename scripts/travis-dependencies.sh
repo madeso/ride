6208b5f39d9d9276f704768b12d93d382d177c47
Gustav <sir.gustav.the.coder@gmail.com>
2016-05-29 18:05:20 +0200

    start using sudo again

--- scripts/travis-dependencies.sh
+++ scripts/travis-dependencies.sh
@@ -2,11 +2,10 @@
 set -ev
 
 # update and display enviroment variables
-export LD_LIBRARY_PATH=$TRAVIS_BUILD_DIR/deps/share/:$LD_LIBRARY_PATH:/usr/local/lib
-export LD_RUN_PATH=$TRAVIS_BUILD_DIR/deps/share/:$LD_RUN_PATH:/usr/local/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export LD_RUN_PATH=$LD_RUN_PATH:/usr/local/lib
 echo "$LD_LIBRARY_PATH"
 echo "$LD_RUN_PATH"
-echo "$PATH"
 
 ls
 
@@ -15,19 +14,21 @@ ls
 usr=$(env | grep SUDO_USER | cut -d= -f 2)
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+  sudo apt-get update -qq
+  sudo apt-get install -qq --assume-yes libgtk2.0-dev libwebkit-dev
+
   # update cmake and compiler: copied from install part of https://github.com/skystrife/cpptoml/blob/toml-v0.4.0/.travis.yml
-  # sudo apt-get install libc6-i386
-  mkdir $TRAVIS_BUILD_DIR/deps/
-  cd $TRAVIS_BUILD_DIR/deps/
-  # wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.2-Linux-i386.sh
-  # sh cmake-3.5.2-Linux-i386.sh --prefix=$TRAVIS_BUILD_DIR/deps/ --exclude-subdir
-  # cd bin
-  # find | sed 's|[^/]*/|- |g'
+  sudo apt-get install libc6-i386
+  wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.2-Linux-i386.sh
+  sudo sh cmake-3.5.2-Linux-i386.sh --prefix=/usr/local --exclude-subdir
 
   # credit: https://github.com/beark/ftl/
   # install g++ 4.8, if tests are run with g++
   if [ "`echo $CXX`" == "g++" ]; then
-    # sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test;
+    sudo apt-get update -qq;
+    sudo apt-get install -qq g++-4.8;
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
     export CXX="g++-4.8" CC="gcc-4.8"
   fi
 fi
@@ -38,7 +39,9 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   cd $TRAVIS_BUILD_DIR/osx-deps/
   wget --no-check-certificate https://cmake.org/files/v3.5/cmake-3.5.2-Darwin-x86_64.tar.gz
   tar -xzf cmake-3.5.2-Darwin-x86_64.tar.gz
-  mv cmake-3.5.2-Darwin-x86_64 cmake
+  cd cmake-3.5.2-Darwin-x86_64
+  ls
+  export PATH=$PATH:/$TRAVIS_BUILD_DIR/osx-deps/cmake-3.5.2-Darwin-x86_64/CMake.app/Contents/bin
 fi
 
 # display the cmake version
@@ -54,10 +57,10 @@ cd pb/
 pwd
 ls
 autoreconf -i
-./configure  --prefix=$TRAVIS_BUILD_DIR/deps/ --disable-shared &> config.log|| cat config.log
+./configure  --disable-shared &> config.log|| cat config.log
 make &> proto_build_log || cat proto_build_log
 make check &> proto_check || cat proto_check
-make install
+sudo make install
 
 # build wxWidgtets
 cd $TRAVIS_BUILD_DIR
@@ -68,8 +71,8 @@ tar -xzf wx.tar.gz &> wxtar || cat wxtar
 cd wxWidgets-3.1.0
 mkdir gtk-build
 cd gtk-build
-../configure --prefix=$TRAVIS_BUILD_DIR/deps/ --enable-webview --disable-compat28
+../configure --enable-webview --disable-compat28
 make
-make install
+sudo make install
 wx-config --version
 
