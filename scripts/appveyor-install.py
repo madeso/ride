340fe6f966c7c6b32bd945beb0559caaf45c44f7
Gustav <sir.gustav.the.coder@gmail.com>
2016-06-14 06:37:58 +0200

    improved script

--- scripts/appveyor-install.py
+++ scripts/appveyor-install.py
@@ -6,13 +6,16 @@ import zipfile
 root = os.getcwd()
 build = os.path.join(root, 'build')
 wxbuild = os.path.join(build, 'wx')
+localwxzip = os.path.join(build, "wx.zip")
+wxsln = os.path.join(wxbuild, 'build', 'msw', 'wx_vc14.sln')
+print root
+print wxsln
 
 wxurl = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.zip"
-localwxzip = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.zip"
+
 urllib.urlretrieve (wxurl, os.path.join(build, localwxzip))
 
 with zipfile.ZipFile(localwxzip, 'r') as z:
     z.extractall(wxbuild)
 
-wxsln = os.path.join(wxbuild, 'build', 'msw', 'wx_vc14.sln')
 os.system('msbuild ' + wxsln)
