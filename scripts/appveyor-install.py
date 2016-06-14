6e62057b7554921ac1f2fab978e7a2701a3dba83
Gustav <sir.gustav.the.coder@gmail.com>
2016-06-14 23:41:13 +0200

    added protobuf

--- scripts/appveyor-install.py
+++ scripts/appveyor-install.py
@@ -16,22 +16,33 @@ if os.environ['PLATFORM'] == 'x86':
 
 root = os.getcwd()
 build = os.path.join(root, 'install-dist')
+
+wxurl = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.zip"
 wxbuild = os.path.join(build, 'wx')
 localwxzip = os.path.join(build, "wx.zip")
 wxsln = os.path.join(wxbuild, 'build', 'msw', 'wx_vc14.sln')
 wxmsbuildcmd = 'msbuild /p:Configuration=Release /p:Platform="{platform}" {solution}'.format(platform=platform, solution=wxsln)
+
+protoulr = "https://github.com/google/protobuf/archive/v2.6.1.zip"
+protobuild = os.path.join(build, 'proto')
+localprotozip = os.path.join(build, 'proto.zip')
+protosln = os.path.join(protobuild, 'protobuf-2.6.1', 'vsprojects', 'protobuf.sln')
+protomsbuildcmd = 'msbuild /p:Configuration=Release /p:Platform="{platform}" {solution}'.format(platform=platform, solution=protosln)
 
 print root
 print wxsln
 
 os.makedirs(build)
 os.makedirs(wxbuild)
-
-wxurl = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.zip"
+os.makedirs(protobuild)
 
 urllib.urlretrieve (wxurl, os.path.join(build, localwxzip))
+urllib.urlretrieve (protoulr, os.path.join(build, localprotozip))
 
 with zipfile.ZipFile(localwxzip, 'r') as z:
     z.extractall(wxbuild)
+
+with zipfile.ZipFile(localprotozip, 'r') as z:
+    z.extractall(protobuild)
 
 os.system(wxmsbuildcmd)
