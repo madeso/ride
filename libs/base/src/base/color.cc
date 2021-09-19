48bd87ea080363d325babda954758c44eb6f4f72
Gustav <sir.gustav.the.coder@gmail.com>
2021-09-19 19:51:17 +0200

    fix: fixed bugg and added chex helper function

--- libs/base/src/base/color.cc
+++ libs/base/src/base/color.cc
@@ -7,7 +7,7 @@ Color Color::rgb(std::uint8_t r, std::uint8_t g, std::uint8_t b, std::uint8_t a)
 
 std::uint8_t get(std::uint32_t c, int index)
 {
-    const auto s = index == 0? c : c << (index*8);
+    const auto s = index == 0? c : c >> (index*8);
     return static_cast<std::uint8_t>(0xFF & s);
 }
 
@@ -16,7 +16,15 @@ Color Color::from_hex_with_alpha(std::uint32_t r)
     return {get(r, 3), get(r, 2), get(r, 1), get(r, 0)};
 }
 
-Color Color::from_hex(std::uint32_t r)
+Color Color::from_hex(std::uint32_t hex)
 {
-    return {get(r, 2), get(r, 1), get(r, 0), 255};
+    const auto r = get(hex, 2);
+    const auto g = get(hex, 1);
+    const auto b = get(hex, 0);
+    return {r, g, b, 255};
+}
+
+Color chex(std::uint32_t r)
+{
+    return Color::from_hex(r);
 }
