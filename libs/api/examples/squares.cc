114a81c3bc1c3e7e35f08494eeb59cb2f9c44285
Gustav <sir.gustav.the.coder@gmail.com>
2021-09-19 20:55:31 +0200

    feat: improved rect functions with rect-cut algorithm

--- libs/api/examples/squares.cc
+++ libs/api/examples/squares.cc
@@ -16,9 +16,34 @@ struct SquaresApp : App
     {
         cache->draw_rect(Rect::from_size(size), chex(open_color::white));
 
-        cache->draw_rect(Rect{{50, 100}, Size{10, 10}}, chex(open_color::red_500));
-        cache->draw_rect(Rect{{100, 50}, Size{10, 10}}, chex(open_color::blue_500));
-        cache->draw_rect(Rect{{100, 100}, Size{100, 100}}, chex(open_color::teal_500));
+        cache->draw_rect(Rect{{50, 100}, Size{30, 30}}, chex(open_color::red_500));
+        cache->draw_rect(Rect{{100, 50}, Size{30, 30}}, chex(open_color::blue_500));
+        
+        auto r = Rect{{100, 100}, Size{400, 400}};
+        
+        const auto size = 20;
+
+        {
+            auto title = r.cut_top(size);
+            for(const auto g: open_color::gray)
+            {
+                cache->draw_rect(title.cut_left(size), chex(g));
+            }
+            cache->draw_rect(title, chex(open_color::pink_400));
+        }
+
+        {
+            auto status = r.cut_bottom(size);
+            for(const auto g: open_color::cyan)
+            {
+                cache->draw_rect(status.cut_right(size), chex(g));
+            }
+            cache->draw_rect(status, chex(open_color::pink_400));
+        }
+
+        cache->draw_rect(r, chex(open_color::teal_500));
+
+        cache->draw_rect(r.Inset(size), chex(open_color::grape_500));
 
         cache->draw_rect(Rect{mouse, Size{10, 10}}, chex(open_color::black));
     }
--- libs/base/src/base/rect.h
+++ libs/base/src/base/rect.h
@@ -3,6 +3,14 @@
 struct vec2;
 struct Size;
 
+enum class Side
+{
+    left,
+    right,
+    top,
+    bottom
+};
+
 struct Rect
 {
     // top-left
@@ -40,6 +48,11 @@ struct Rect
     int get_right() const;
     int get_bottom() const;
 
+    void set_left(int new_left);
+    void set_top(int new_top);
+    void set_right(int new_right);
+    void set_bottom(int new_bottom);
+
     static bool overlap(Rect a, Rect b);
     static Rect intersect(Rect a, Rect b);
     static Rect merge(Rect a, Rect b);
@@ -48,10 +61,20 @@ struct Rect
 
     bool Contains(const vec2& p) const;
 
-    Rect CreateWestFromMaxSize(int max_size) const;
-    Rect CreateNorthFromMaxSize(int max_size) const;
-    Rect CreateSouthFromMaxSize(int max_size) const;
-    Rect CreateEastFromMaxSize(int max_size) const;
+    static Rect FromL(int left, int top, int right, int bottom);
+
+    Rect cut_left(int a);
+    Rect cut_right(int a);
+    Rect cut_top(int a);
+    Rect cut_bottom(int a);
+
+    Rect get_cut_left(int a) const;
+    Rect get_cut_right(int a) const;
+    Rect get_cut_top(int a) const;
+    Rect get_cut_bottom(int a) const;
+
+    Rect cut(Side side, int a);
+    Rect get_cut(Side side, int a) const;
 
     Rect Offset(const vec2& offset) const;
 
